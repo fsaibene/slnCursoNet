a77d3a3b4cff502bcc424b1d400ef7bb896a065e
Fsaibene <fernandomsaibene@hotmail.com>
2017-09-26 13:00:16 -0300

    Se agregaron entidades

---
+++ Entidades/ClienteEmpresa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class ClienteEmpresa
+    {
+        public string Nombre { get; set; }
+        public string Cuit { get; set; }
+        public string Contacto { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+    }
+}
---
+++ Entidades/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioCosto { get; set; }
+        public double Margen { get; set; }
+        public double IVA { get; set; }
+        public decimal PrecioBruto { get; //TODO: Implementar get Formatted
+        }
+        public decimal PrecioVenta { get; }
+        public string Proveedor { get; set; }
+        public string Categoria { get; set; }
+        public string SubCategoria { get; set; }
+    }
+}
